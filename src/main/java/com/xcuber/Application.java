ba816044e49752707306f11c26072ebdd52a7615
junyan <junyan@DESKTOP-1ICR7KA>
2017-08-17 21:32:37 +0800

    [add]add some dependecies

--- src/main/java/com/xcuber/Application.java
+++ src/main/java/com/xcuber/Application.java
@@ -1,22 +1,18 @@
 package com.xcuber;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @RestController
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter implements EmbeddedServletContainerCustomizer {
+public class Application extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
-
-	@Override
-	public void customize(ConfigurableEmbeddedServletContainer container) {
-		container.setPort(8088);
+		SpringApplication application = new SpringApplication(Application.class);	
+		application.setBannerMode(Banner.Mode.CONSOLE);
+		application.run(args);
 	}
 }
--- src/main/java/com/xcuber/controller/HomeController.java
+++ src/main/java/com/xcuber/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.xcuber.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,6 +11,9 @@ public class HomeController {
 
 	@GetMapping()
 	public String home() {
-		return "Welcome!";
+		return "Welcome!" + name;
 	}
+	
+	@Value("${mine.name}")
+	private String name;
 }
