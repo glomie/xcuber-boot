9fe3baac480f885cf5a643f8fd07d3042e5fb320
hzwjy <hzwjy@192.168.56.1>
2017-08-18 09:55:45 +0800

    make security false

--- src/main/java/com/xcuber/Application.java
+++ src/main/java/com/xcuber/Application.java
@@ -3,16 +3,13 @@ package com.xcuber;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@RestController
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication application = new SpringApplication(Application.class);	
-		application.setBannerMode(Banner.Mode.CONSOLE);
+		application.setBannerMode(Banner.Mode.LOG);
 		application.run(args);
 	}
 }
