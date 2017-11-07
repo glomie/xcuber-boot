9625606899bf52687804c6debff352e379885a14
wujunyan <wujunyan>
2017-11-07 19:13:44 +0800

    [modify]修改recourse包

--- src/main/java/com/xcuber/controller/HomeController.java
+++ src/main/java/com/xcuber/controller/HomeController.java
@@ -1,6 +1,5 @@
 package com.xcuber.controller;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +13,5 @@ public class HomeController {
 		return "Welcome!" + name;
 	}
 	
-	@Value("${mine.name}")
 	private String name;
 }
